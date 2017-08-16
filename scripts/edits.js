da2793af468cd54d32aa30f975cec21727572dd4
Andrew <andrewchoi1@hotmail.com>
2017-08-16 12:44:44 +0700

    assignment14 finished

---
+++ scripts/edits.js
@@ -0,0 +1,14 @@
+ var findParentByClassName = function(element, targetClass) {
+    if (element) {
+        var currentParent = element.parentElement;
+        while (currentParent.className !== targetClass && currentParent.className !== null) {
+            currentParent = currentParent.parentElement;
+
+             return element;
+
+        } else { console.log("No Parent Found");
+
+       }
+
+    }
+};
